15c95be2473ac6b7bc329ba2d4256133c044492c
igor magalhaes <magalhaesigor824@gmail.com>
2026-04-22 08:11:29 -0300

    teste

--- assignments/06-projects/02-bank/submissions/Matheus_coelho/src/Idpbank.java
+++ assignments/06-projects/02-bank/submissions/Matheus_coelho/src/Idpbank.java
@@ -1,3 +1,23 @@
-public class Idpbank {
-    
+public class IDPBank {
+    public static void main(String[] args) {
+        // Lógica do menu principal e fluxo do programa
+    }
+
+    public static Customer createCustomer(String firstName, String lastName, String cpf) {
+        return new Customer(firstName, lastName, cpf);
+    }
+
+    public static Account openAccount(Customer customer) {
+        Account account = new Account();
+        customer.addAccount(account);
+        return account;
+    }
+
+    public static void deposit(Account account, double amount) {
+        account.deposit(amount);
+    }
+
+    public static void withdraw(Account account, double amount) {
+        account.withdraw(amount);
+    }
 }
